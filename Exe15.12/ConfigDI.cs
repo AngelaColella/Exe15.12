5d90e101b54243f89e2b92e5a41fb2870a98972e
angela.colella <angela.colella@WINAPIUZIYVIF6L>
2020-12-15 15:33:18 +0100

    Final Commit

--- Exe15.12/ConfigDI.cs
+++ Exe15.12/ConfigDI.cs
@@ -8,14 +8,14 @@ namespace Exe15._12
 {
     public class ConfigDI
     {
-        // va recuperata la connection string
+        //// va recuperata la connection string
         
-        public static ServiceProvider DIConfiguration()
-        {
-            return new ServiceCollection()
-                .AddTransient<BusinessLayer>()             
-                .AddTransient<IRepository<Product>, ProductRepository>()
-                .BuildServiceProvider();
-        }
+        //public static ServiceProvider DIConfiguration()
+        //{
+        //    return new ServiceCollection()
+        //        .AddTransient<BusinessLayer>()             
+        //        .AddTransient<IRepository<Product>, ProductRepository>()
+        //        .BuildServiceProvider();
+        //}
     }
 }
